f346c5b5d2572f94bc9bcb90e5474ca0f1008d7f
Ivan Kurmanov <duraley@gmail.com>
2013-04-28 00:03:13 +0300

    license: MIT

--- api.js
+++ api.js
@@ -3,9 +3,32 @@
 // an experimental, prototype jquery-like library-wrapper
 // around AWS SDK for node.js
 //
+//   https://github.com/kurmanka/aws-jq
 //   http://aws.amazon.com/sdkfornodejs/ 
 //   https://github.com/aws/aws-sdk-js
-// (c) 2013-04-20, Ivan Kurmanov
+/*
+
+Copyright (c) 2013, Ivan Kurmanov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ 
+*/
 
 var AWS = require('aws-sdk');
 var ec2; 
