9b4c323a17349f55e318bea95effd2b931a1b70c
Ivan Kurmanov <duraley@gmail.com>
2013-05-08 15:38:14 +0300

    first round of splitting code into separate files

--- api.js
+++ api.js
@@ -30,258 +30,14 @@ THE SOFTWARE.
  
 */
 
+
 var AWS = require('aws-sdk');
-var ec2; 
 var async = require('async');
 
-// this checks the selector string, parses it
-// and identifies the object type and the object's id
+var jq = require( './lib/jq.js' );
+jq.ec2 = require('./lib/ec2.js');
 
-function parse_selector (s) {
-	var r = { type: null, id: null }; // return structure
-
-	var instance_re = /^\s*(i-[0-9a-fA-F]{8})\s*$/;
-	var volume_re   = /^\s*(vol-[0-9a-fA-F]{8})\s*$/;
-	var match = s.match(instance_re);
-	if (match) {
-		r.type = 'instance';
-		r.id   = match[0];
-		return r;
-	}
-	match = s.match(volume_re);
-	if (match) {
-		r.type = 'volume';
-		r.id   = match[0];
-		return r;
-	}
-	// none of the above
-	return null;
-}
-
-function get_volume_details(v,cb) {
-	var request = ec2.describeVolumes( { VolumeIds: [v] } );
-	request.on('success', function(response) {
-	//	console.log('get_volume_details: success');
-		var details = response.data.Volumes[0];
-		cb(details);
-	});
-	request.on('error', function(response) {
-		console.log('get_volume_details: error');
-		cb(null);
-	});
-
-	request.send();
-}
-
-function get_instance_details(i,cb) {
-//	console.log('get_instance_details('+i+'): start');
-	var request = ec2.describeInstances( { InstanceIds: [i] } );
-
-	// register callbacks on request to retrieve response data
-	request.on('success', function(response) {
-//		console.log('get_instance_details: success');
-		var details = response.data.Reservations[0].Instances[0];
-		//console.log(details);
-		cb(details);
-	});
-
-	// register callbacks on request to retrieve response data
-	request.on('error', function(response) {
-//		console.log('get_instance_details: error');
-	});
-
-	request.send();
-}
-
-function instance_info_get(id,args,s,f) {
-//	console.log( 'id: ', id)
-	var params = Array.prototype.slice.apply(args);
-	console.log(params);
-	var cb = params.pop();
-
-	get_instance_details(id,function(data) {
-		if(!data) {return f();}
-
-//		console.log(data);
-		var values = [];
-		for (var i = 0; i < params.length; i++) {
-			var key = params[i];
-			//console.log( 'param['+i+']: ', key, ' = ', data[key]);
-			values[i] = data[key];
-		}
-//		console.log('values: ', values);
-		values.push(s);
-		cb.apply(this,values);
-	});
-}
-
-function volume_info_get(id,args,s,f){
-//	console.log( 'id: ', id)
-	var params = Array.prototype.slice.apply(args);
-//	console.log(params);
-	var cb = params.pop();
-
-	get_volume_details(id,function(data) {
-		if(!data) {return f();}
-
-//		console.log(data);
-		var values = [];
-		for (var i = 0; i < params.length; i++) {
-			var key = params[i];
-			//console.log( 'param['+i+']: ', key, ' = ', data[key]);
-			values[i] = data[key];
-		}
-//		console.log('values: ', values);
-		values.push(s);
-		cb.apply(this,values);
-	});
-
-};
-
-var wait = 2000; // milliseconds
-// wait until instance id is in state state
-function until_instance_state(id,state,done){
-	var recursive = function () {
-		console.log("... waiting for " + state);
-		get_instance_details(id, function(d) {
-			console.log( d.State.Name );
-			if(d.State.Name==state) {
-				done(null,d);
-			} else {
-				setTimeout(recursive,wait);
-			}
-		});
-	}
-	recursive();
-}
-
-// wait until volume id is in state state
-function until_volume_state(id,state,done){
-	var recursive = function () {
-		console.log("... waiting for " + state);
-		get_volume_details(id, function(d) {
-			console.log( d.State );
-			if(d.State==state) {
-				done(null,d);
-			} else {
-				setTimeout(recursive,wait);
-			}
-		});
-	}
-	recursive();
-}
-
-// wait until volume id is in attachment with state state
-function until_volume_attachment_state(id,state,done){
-	var recursive = function () {
-		console.log("... waiting for " + state);
-		get_volume_details(id, function(d) {
-			if( d && d.Attachments ) { console.log( d.Attachments[0].State ); }
-
-			if( d && d.Attachments 
-				&& d.Attachments[0].State==state ) {
-				done(null,d);
-			} else {
-				setTimeout(recursive,wait);
-			}
-		});
-	}
-	recursive();
-}
-
-
-// attach an EBS volume to an EC2 instance
-function attach(params,s,f) {
-	//	console.log( 'attach(): start' );
-	if (!this._) {
-		// no object to do start on
-		f('no object to run on');
-	};
-	// see if this._ points to an instance
-	// ... selector( this._ );
-	var inst;
-	var vol;
-	var device;
-
-	var item = parse_selector( this._ );
-
-	if (item && item.type == 'instance') {
-		inst   = item.id;
-		vol    = params.volume;
-		device = params.device;
-
-	} else if (item && item.type == 'volume') {
-		vol    = item.id;
-		inst   = params.instance;
-		device = params.device;
-	}
-
-	if (!item || !vol || !inst || !device) {
-		f( 'i need an instance and a volume and the device name'); 
-		return this; 
-	}
-
-	async.waterfall( [
-		function (done) {
-			// check that the volume is in state 'available'
-			var volume = get_volume_details( vol, function(d) {
-				if (!d) { return done( 'volume is not known: ' + vol); }
-				if (d.State=='available') {
-					done(null);
-				} else {
-					done('volume is not available: ' + d.State);
-				}
-			});
-		},
-		function (done) {
-			ec2.attachVolume({InstanceId:inst, VolumeId:vol, Device:device}, function(err,data) {
-				if (err) { console.log( 'attachVolume error: ' + err ); f(err); }
-				// wait 
-				until_volume_attachment_state( vol, 'attached',
-					// could be more cautious here XXX 
-					function(e,d) { s(); }
-				);
-			});
-			console.log( 'attachVolume() request sent' );
-		}
-	], function (err,d) {
-		console.log( 'failed waterfall', err );
-		if(err) { f(err); }
-	});
-}
-
-// detach an EBS volume from an ec2 instance
-function detach(params,s,f) {
-	// stub
-	var vol = params.volume;
-	console.log( 'asked to detach ' + vol );
-
-	async.waterfall( [
-		function (done) {
-			// check that the volume is in state 'available'
-			var volume = get_volume_details( vol, function(d) {
-				if (!d) { return done( 'volume is not known: ' + vol); }
-				if (d.State=='in-use') {
-					done(null);
-				} else {
-					done('volume is not attached: ' + d.State);
-				}
-			});
-		},
-		function (done) {
-			ec2.detachVolume({VolumeId:vol}, function(err,data) {
-				if (err) { console.log( 'detachVolume error: ' + err ); f(err); }
-				// wait 
-				// could be more cautious XXX
-				until_volume_state( vol, 'available', function(e,d) { s(); }) 
-			});
-			console.log( 'detachVolume() request sent' );
-		}
-	], function (err,d) {
-		console.log( 'failed waterfall', err );
-		if(err) { f(err); }
-	});
-}
+var ec2;
 
 
 // start an EC2 instance (assuming to be a stopped instance)
@@ -297,7 +53,7 @@ function start(s,f) {
 	ec2.startInstances({InstanceIds:[iid]}, function(err,data) {
 		if (err) { console.log( 'startInstances error: ' + err ); f(err); }
 		// wait 
-		until_instance_state( iid, 'running', function(e,d) {
+		jq.ec2.until_instance_state( iid, 'running', function(e,d) {
 			s();
 		})
 	});
@@ -317,7 +73,7 @@ function stop(s,f) {
 	ec2.stopInstances({InstanceIds:[iid]}, function(err,data) {
 		if (err) { console.log( 'stopInstances error: ' + err ); f(err); }
 		// wait 
-		until_instance_state( iid, 'stopped', function(e,d) {
+		jq.ec2.until_instance_state( iid, 'stopped', function(e,d) {
 			s();
 		})
 	});
@@ -340,13 +96,13 @@ function _attach(p,dev){
 		&& dev) { 
 		p = {volume: p, device: dev};
 	}
-	return this.then( attach, p );
+	return this.then( jq.ec2.attach, p );
 }
 
 function _detach(p){
-	var i = parse_selector(p || this._);
+	var i = jq.parse_selector(p || this._);
 	if (i && i.type == 'volume') {
-		this.then( detach, {volume: i.id} )		
+		this.then( jq.ec2.detach, {volume: i.id} )		
 	} else {
 		console.log( 'Aaaaaaa! detach what?!' );
 	}
@@ -355,14 +111,14 @@ function _detach(p){
 }
 
 function _get() {
-	var i = parse_selector(this._);
+	var i = jq.parse_selector(this._);
 
 	if (i && i.type == 'instance') {
-		return this.then( instance_info_get, i.id, arguments );
+		return this.then( jq.ec2.instance_info_get, i.id, arguments );
 	}
 
 	if (i && i.type == 'volume') {
-		return this.then( volume_info_get, i.id, arguments );
+		return this.then( jq.ec2.volume_info_get, i.id, arguments );
 	} 
 
 //	if (!i || !i.type) {
@@ -433,6 +189,7 @@ function aws( selector ) {
 
 	if(!ec2) {
 		ec2 = new AWS.EC2;
+		jq.ec2.setup( {}, ec2 );
 	}
 
 	var o = {
@@ -460,5 +217,5 @@ function _init( config ) {
 
 // export aws function
 exports.aws = aws;
-exports.get_volume_details   = get_volume_details;
-exports.get_instance_details = get_instance_details;
+exports.get_volume_details   = jq.ec2.get_volume_details;
+exports.get_instance_details = jq.ec2.get_instance_details;
